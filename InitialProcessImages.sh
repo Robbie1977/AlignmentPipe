569f48df0754d4d16f0f3beff86b583eda74b673
Robert Court <robbie.court@gmail.com>
2013-10-10 15:09:03 +0100

    Added PreProcessing call

--- InitialProcessImages.sh
+++ InitialProcessImages.sh
@@ -2,27 +2,22 @@
 
 set -- $(<karenin.var)
 
-Ij=${10}
-lsm2nrrd=${20}
-inbox=${2}
 proc=${3}
-ppro=${21}
-chn=${30}
+cmtk=${11}
 
-for f in $inbox
+for f in $proc*-PP_C3.nrrd
 do
 	echo 'Processing $f'
-	$Ij -macro $lsm2nrrd $f -batch
+	echo 'Initial aligment:'
+	$cmtk
 	fr=`echo $f | rev | cut -c 4- |rev`
-	if [ -e $fr*$chn.nrrd ] #note last channel number! 
+	if [ -e $fr*C3.nrrd ] #note last channel number! 
 	then
 		echo Successfully converted $f to `ls $fr*.nrrd | wc -l` NRRD files
-		echo 'PreProccessing image channels:'
-		python $ppro $fr-PP_C1.nrrd $fr-PP_C2.nrrd C 10 
 		echo 'Cleaning...'
 		rm $f
 		mv $fr*.nrrd $processing
 		echo 'Pushed for processing...'
 	fi
 done		
-		
+	
