4cb1e6956661de7d43b4e8f17f36e2af3821951f
Robert Court <robbie.court@gmail.com>
2013-10-10 19:36:22 +0100

    Saving work to date - Still in progress

--- InitialProcessImages.sh
+++ InitialProcessImages.sh
@@ -3,20 +3,23 @@
 set -- $(<settings.var)
 
 proc=${3}
-cmtk=${11}
+cmtkdir=${11}
+Tfile=${29}
 
-for f in $proc*-PP_C3.nrrd
+for f in $proc*BG.nrrd
 do
 	echo 'Processing $f'
 	echo 'Initial aligment:'
-	$cmtk
+    inl=`echo $proc | wc -c`
+    fr=`echo $f | rev | cut -c 6- |rev | cut -c $inl-`
+	${cmtkdir}make_initial_affine --principal-axes ${Tfile} ${fr}.nrrd
 	fr=`echo $f | rev | cut -c 4- |rev`
 	if [ -e $fr*C3.nrrd ] #note last channel number! 
 	then
 		echo Successfully converted $f to `ls $fr*.nrrd | wc -l` NRRD files
 		echo 'Cleaning...'
 		rm $f
-		mv $fr*.nrrd $processing
+		mv $fr*.nrrd $proc
 		echo 'Pushed for processing...'
 	fi
 done		
