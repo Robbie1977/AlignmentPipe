c1efd96b06c9d1f5b1bb61944bc6f9c195361744
Robert Court <robbie.court@gmail.com>
2014-01-24 13:03:11 +0000

    Added script to correct error in secondary channel allocation
    Sorts out which is NG and which is SG by assuming the summed intensity
    of the NG channel is greater.

---
+++ SortNG-SG.py
@@ -0,0 +1,32 @@
+import numpy as np
+import sys
+import nrrd
+
+if (len(sys.argv) < 3):
+    print 'Error: missing arguments!'
+    print 'e.g. python SortNG-SG.py NG.nrrd SG.nrrd'
+else:
+
+    print 'Checking intensity for ', str(sys.argv[1]), ' against the intensity of ', str(sys.argv[2]), '...'
+  
+    readdata, op1 = nrrd.read(str(sys.argv[1]))
+    
+    im1 = readdata
+    
+    readdata, op2 = nrrd.read(str(sys.argv[2])) 
+    im2 = readdata
+      
+    if (im1.size <> im2.size):
+        print '\n\nError: Images must be the same size!!'
+    else:
+        
+        if (im1.sum < im2.sum):
+            print 'swapping the files due to inconsistency in assignment...'
+            nrrd.write(str(sys.argv[1]), im2, options=op2)
+            nrrd.write(str(sys.argv[2]), im1, options=op1)
+        else:
+            print 'images appear to be correctly assigned'
+        print 'Done.'
+        
+  
+
