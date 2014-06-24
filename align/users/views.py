dd5f2644e042f01cdb975f3aaf37db127886aa85
Robert Court <robbie.court@gmail.com>
2014-06-24 01:35:56 +0100

    error catching for unknown user

--- align/users/views.py
+++ align/users/views.py
@@ -4,7 +4,10 @@ from django.template.context import RequestContext
 def home(request):
   if not request.user == '':
     from users.models import User
-    fn = User.objects.filter(username=request.user).values('first_name', 'last_name')[0]
+    try:
+      fn = User.objects.filter(username=request.user).values('first_name', 'last_name')[0]
+    except:
+      fn = {'first_name':'Unknown','last_name':'User'}
     context = RequestContext(request,
                            {'request': request,
                             'user': request.user}.update(fn))
