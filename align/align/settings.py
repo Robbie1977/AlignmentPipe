d132747f62a5f4fddf0d9a08144d8665360338b7
Robert Court <robbie.court@gmail.com>
2014-07-04 12:24:34 +0100

    covering missing server settings

--- align/align/settings.py
+++ align/align/settings.py
@@ -6,6 +6,12 @@ con = psycopg2.connect(host='bocian.inf.ed.ac.uk', database='alignment', user='a
 cur = con.cursor()
 cur.execute("SELECT upload_dir FROM system_server WHERE host_name like '" + host + "'")
 record = cur.fetchone()
+if record = None:
+  print 'Missing server settings for ' + str(host)
+  cur.execute("SELECT upload_dir, host_name FROM system_server")
+  record = cur.fetchone()
+  print 'Having to use settings for ' + str(record[1])
+  host = str(record[1])
 uploadfolder = str(record[0])
 cur.close()
 con.close()
