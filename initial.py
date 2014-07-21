3a7d0187bd7f6e439f4109e51b07a37624b3402b
Robert Court <robbie.court@gmail.com>
2014-07-21 21:53:16 +0100

    resolves ignored relationships

--- initial.py
+++ initial.py
@@ -48,7 +48,7 @@ def initial(name, template=template, init_threshold=0.3, bgfile='image_Ch1.nrrd'
 
 if __name__ == "__main__":
   if active and '2' in run_stage:
-    cur.execute("SELECT images_alignment.name, system_template.file, system_setting.initial_pass_level, images_original_nrrd.file, system_setting.cmtk_initial_var, system_setting.cmtk_align_var FROM images_alignment, system_template, system_setting, images_original_nrrd WHERE alignment_stage = 2 AND images_original_nrrd.channel = images_alignment.background_channel AND images_original_nrrd.image_id = images_alignment.id")
+    cur.execute("SELECT images_alignment.name, system_template.file, system_setting.initial_pass_level, images_original_nrrd.file, system_setting.cmtk_initial_var, system_setting.cmtk_align_var FROM images_alignment, system_template, system_setting, images_original_nrrd WHERE alignment_stage = 2 AND images_original_nrrd.channel = images_alignment.background_channel AND images_original_nrrd.image_id = images_alignment.id AND images_alignment.settings_id = system_setting.id AND system_setting.template_id = system_template.id")
     records = cur.fetchall()
     total = len(records)
     count = 0
