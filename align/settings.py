452e6bd1581e5c3af3fc8e6e513c6872e194195b
Robert Court <robbie.court@gmail.com>
2014-07-25 06:28:15 -0400

    clean up

--- align/settings.py
+++ align/settings.py
@@ -141,7 +141,7 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'alignSet.urls'
+ROOT_URLCONF = 'urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'align.wsgi.application'
