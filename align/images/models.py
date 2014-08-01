8d3a9be9f437bcfb1f0ba01a13b8e2d6494993cb
Robert Court <robbie.court@gmail.com>
2014-08-01 01:29:49 +0100

    cleaning up

--- align/images/models.py
+++ align/images/models.py
@@ -16,7 +16,6 @@ conv_orien = dict(zip(comp_orien.values(),comp_orien.keys()))
 # Create your models here.
 
 class Alignment(models.Model):
-    import users.models
     import system.models
     #from users.models import User
     #from system.models import Setting
