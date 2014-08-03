200e84daf1aafb42e632b75a82f597c40f0f9897
Robert Court <robbie.court@gmail.com>
2014-08-03 22:08:31 +0100

    adding crop

--- align/images/models.py
+++ align/images/models.py
@@ -30,6 +30,7 @@ class Alignment(models.Model):
     loading_host = models.CharField(max_length=100, blank=True)
     original_ext = models.CharField(max_length=10)
     original_path = models.TextField(max_length=1000)
+    crop_xyz = models.CharField(max_length=100, blank=True)
     temp_initial_nrrd = models.TextField(max_length=1000, blank=True)
     temp_initial_score = models.CharField(max_length=20, blank=True)
     background_channel = models.IntegerField(choices=chan.items(), default=0)
