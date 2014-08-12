8f3c5be42e8b88c8cb5e2a3fccd24dcaee3a3c8c
Robert Court <robbie.court@gmail.com>
2014-08-12 03:07:16 +0100

    test

--- align/images/models.py
+++ align/images/models.py
@@ -156,8 +156,8 @@ class Mask_original(models.Model):
     def __str__(self):
         return 'Mask for ' + str(self.image)
     def mask_image(self):
-      if self.complete:
-        return '<img src="/images/nrrd/mask_original/%s"/>' % str(self.id)
+      # if self.complete:
+      #   return '<img src="/images/nrrd/mask_original/%s"/>' % str(self.id)
       return '<img src="/static/waiting.gif"/>'
     mask_image.short_description = 'detected objects'
     mask_image.allow_tags = True
@@ -171,8 +171,8 @@ class Mask_aligned(models.Model):
     def __str__(self):
         return 'Mask for aligned ' + str(self.image) + ' ' + str(self.channel).upper() + ' channel'
     def mask_image(self):
-      if self.complete:
-        return '<img src="/images/nrrd/mask_aligned_%s/%s"/>' % str(self.channel), str(self.id)
+      # if self.complete:
+      #   return '<img src="/images/nrrd/mask_aligned_%s/%s"/>' % str(self.channel), str(self.id)
       return '<img src="/static/waiting.gif"/>'
     mask_image.short_description = 'detected objects'
     mask_image.allow_tags = True
