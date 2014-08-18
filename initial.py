75ad7e473c0135dd1f025332bec6feb3e6549b1b
Robert Court <robbie.court@gmail.com>
2014-08-18 09:32:41 +0100

    test

--- initial.py
+++ initial.py
@@ -60,7 +60,7 @@ if __name__ == "__main__":
     records = cur.fetchall()
     total = len(records)
     if total == 0:
-      cur.execute("UPDATE images_alignment SET alignment_stage = 2 WHERE alignment_stage = 2002 ORDER BY id LIMIT 2")
+      cur.execute("UPDATE images_alignment SET alignment_stage = 2 FROM (SELECT id FROM images_alignment WHERE alignment_stage = 2002 ORDER BY id LIMIT 2) s WHERE s.id = images_alignment.id"
       cur.connection.commit()
       gc.collect()
     count = 0
