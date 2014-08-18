ff9b82d10a5912ed59b51b110d5b5742f1b32c98
Robert Court <robbie.court@gmail.com>
2014-08-18 09:36:15 +0100

    syntax fix

--- warp.py
+++ warp.py
@@ -49,7 +49,7 @@ if __name__ == "__main__":
     records = cur.fetchall()
     total = len(records)
     if total == 0:
-      cur.execute("UPDATE images_alignment SET alignment_stage = 4 FROM (SELECT id FROM images_alignment WHERE alignment_stage = 2004 ORDER BY id LIMIT 2) s WHERE s.id = images_alignment.id"
+      cur.execute("UPDATE images_alignment SET alignment_stage = 4 FROM (SELECT id FROM images_alignment WHERE alignment_stage = 2004 ORDER BY id LIMIT 2) s WHERE s.id = images_alignment.id")
       cur.connection.commit()
       gc.collect()
     count = 0
