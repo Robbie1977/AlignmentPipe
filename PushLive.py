492b46bffe093bcd4e2e5710151efb995e8ff0de
Robert Court <robbie.court@gmail.com>
2015-08-05 17:06:16 +0100

    adding id in notes

--- PushLive.py
+++ PushLive.py
@@ -47,7 +47,7 @@ def pushLive(id, name, sgfile):
         print "Creating wlz: " + "/VFB/i/" + first + "/" + last + "/volume.wlz"
         # TBD: resolve voxel size from template.
         subprocess.call("nice " + wlzDir + "WlzExtFFConvert -f tif -F wlz " + tempfolder + "../../IMAGE_DATA/VFB/i/" + first + "/" + last + "/volume.tif |" + wlzDir + "WlzThreshold -v2 |" + wlzDir + "WlzSetVoxelSize -z0.46 -x0.4612588 -y0.4612588 >" + tempfolder + "../../IMAGE_DATA/VFB/i/" + first + "/" + last + "/volume.wlz" , shell=True)
-        cur.execute("UPDATE images_alignment SET alignment_stage = 21 WHERE id = %s", [str(id)])
+        cur.execute("UPDATE images_alignment SET alignment_stage = 21, notes = %s WHERE id = %s", [str("VFB_"+first+last),str(id)])
         cur.connection.commit()
     else:
         print "Skipping " + name
