629797a030759ebae9e71040c14faca6210a3953
Robert Court <robbie.court@gmail.com>
2015-10-11 19:31:41 +0100

    syntax fix

--- align/images/urls.py
+++ align/images/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^details/(?P<image_id>\w+)', views.detail, name='details'),
     url(r'^hist/(?P<image_id>\w+)', views.plotResults, name='histograms'),
-    url(r'^nrrd/(?P<image_type>\w+)/(?P<image_id>\X+)', views.plotNrrd, name='nrrd'),
+    url(r'^nrrd/(?P<image_type>\w+)/(?P<image_id>[0-9,-]+)', views.plotNrrd, name='nrrd'),
     # url(r'^/(?P<image_id>\w+/$)', views.detail, name='detail')
     # url(r'^$', views.index, name='details')
     # url(r'^/staticImage.png$', 'images.views.showStaticImage'),
