5f8ad83f793fb944de03ddd6b2c20808dbc7def4
Robert Court <robbie.court@gmail.com>
2015-10-11 18:20:00 +0100

    correcting sub file name

--- align/images/models.py
+++ align/images/models.py
@@ -238,7 +238,7 @@ class Mask_original(models.Model):
     image_download.allow_tags = True
     def mask_download(self):
       chanFile = str(self.image.file)
-      chanFile = chanFile.replace('.nrrd','-objMask.nrrd')
+      chanFile = chanFile.replace('.nrrd','-objMask.nrrd').replace('.nrrd',str(self.id) + '.nrrd')
       return '<a href="/static/downloads/%s"/>%s</a>' % (chanFile, chanFile)
     mask_download.short_description = 'download image mask'
     mask_download.allow_tags = True
