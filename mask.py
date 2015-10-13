474580df8aabfdb9b1e1bd0b7b2a7f1dd78da4db
Robert Court <robbie.court@gmail.com>
2015-10-13 11:42:48 +0100

    replicating to crop

--- mask.py
+++ mask.py
@@ -45,6 +45,10 @@ if __name__ == "__main__":
       modfile = str(line[2]).replace('.nrrd','-ModFile.nrrd').replace('.nrrd', str(line[0]) + '.nrrd')
       if not os.path.isfile(tempfolder + modfile):
           shutil.copyfile(tempfolder + str(line[2]),tempfolder + modfile)
+      if not line[6]:
+          oldName = str(line[7])
+          newName = str(line[7]) + "_ModByMask" + str(line[0])
+          shutil.copyfile(tempfolder + modfile, tempfolder + str(line[2]).replace(oldName, newName))
       cutObj(tempfolder + modfile, tempfolder + maskfile, labels=str(line[1]))
       cur.execute("UPDATE images_mask_original SET cut_complete=True WHERE id = %s ", [str(line[0])])
       cur.connection.commit()
@@ -59,8 +63,6 @@ if __name__ == "__main__":
       else:
         print 'Creating new alignment record with results...'
         print "Old ID: " + str(oldId)
-        oldName = str(line[7])
-        newName = str(line[7]) + "_ModByMask" + str(line[0])
         cur.execute("INSERT INTO images_alignment(name, settings_id, max_stage, last_host, alignment_stage, orig_orientation, loading_host, original_ext, original_path, crop_xyz, background_channel, signal_channel, ac1_channel, notes, reference, user_id) SELECT %s, settings_id, 2, last_host, alignment_stage, orig_orientation, loading_host, original_ext, original_path, crop_xyz, background_channel, signal_channel, ac1_channel, notes, reference, user_id FROM images_alignment WHERE id = %s", [newName, oldId])
         cur.connection.commit()
         gc.collect()
@@ -81,7 +83,6 @@ if __name__ == "__main__":
             if (str(line[2]) == str(fl[0]).replace(newName, oldName)):
                 newOrig = fl[1]
                 print 'file matched'
-        shutil.copyfile(tempfolder + modfile, tempfolder + str(line[2]).replace(oldName, newName))
         os.rename(tempfolder + modfile, tempfolder + str(modfile).replace(oldName, newName))
         os.rename(tempfolder + maskfile, tempfolder + str(maskfile).replace(oldName, newName))
         print 'Switching to new alignment via ' + str(newOrig)
@@ -103,7 +104,7 @@ if __name__ == "__main__":
     print 'inactive or stage 0 not selected'
 
   if active and '0' in run_stage:
-    cur.execute("SELECT images_mask_original.id, images_mask_original.crop_objects, images_original_nrrd.file, images_mask_original.auto_restart_alignment, images_alignment.id, images_original_nrrd.id FROM images_mask_original, images_original_nrrd, images_alignment WHERE images_original_nrrd.id = images_mask_original.image_id AND images_original_nrrd.image_id = images_alignment.id AND images_mask_original.complete = True AND images_mask_original.crop_complete = False AND images_mask_original.crop_objects is not null AND images_mask_original.crop_objects != '' AND images_mask_original.crop_objects != '{}' ORDER BY images_mask_original.id")
+    cur.execute("SELECT images_mask_original.id, images_mask_original.crop_objects, images_original_nrrd.file, images_mask_original.auto_restart_alignment, images_alignment.id, images_original_nrrd.id, images_mask_original.overwrite_original, images_alignment.name FROM images_mask_original, images_original_nrrd, images_alignment WHERE images_original_nrrd.id = images_mask_original.image_id AND images_original_nrrd.image_id = images_alignment.id AND images_mask_original.complete = True AND images_mask_original.crop_complete = False AND images_mask_original.crop_objects is not null AND images_mask_original.crop_objects != '' AND images_mask_original.crop_objects != '{}' ORDER BY images_mask_original.id")
     records = cur.fetchall()
     total = len(records)
     count = 0
@@ -115,23 +116,59 @@ if __name__ == "__main__":
       modfile = str(line[2]).replace('.nrrd','-ModFile.nrrd').replace('.nrrd', str(line[0]) + '.nrrd')
       if not os.path.isfile(tempfolder + modfile):
           shutil.copyfile(tempfolder + str(line[2]),tempfolder + modfile)
+      if not line[6]:
+          oldName = str(line[7])
+          newName = str(line[7]) + "_ModByMask" + str(line[0])
+          shutil.copyfile(tempfolder + modfile, tempfolder + str(line[2]).replace(oldName, newName))
       cropObj(tempfolder + modfile, tempfolder + maskfile, labels=str(line[1]))
       cur.execute("UPDATE images_mask_original SET crop_complete=True WHERE id = %s ", [str(line[0])])
       cur.connection.commit()
       gc.collect()
+      newId = str(line[4])
+      oldId = str(line[4])
+      if line[6]:
+          print 'Updating with results...'
+          cur.execute("UPDATE images_original_nrrd SET file=%s WHERE id = %s ", [modfile, str(line[5])])
+          cur.connection.commit()
+          gc.collect()
+      else:
+        print 'Creating new alignment record with results...'
+        print "Old ID: " + str(oldId)
+        cur.execute("INSERT INTO images_alignment(name, settings_id, max_stage, last_host, alignment_stage, orig_orientation, loading_host, original_ext, original_path, crop_xyz, background_channel, signal_channel, ac1_channel, notes, reference, user_id) SELECT %s, settings_id, 2, last_host, alignment_stage, orig_orientation, loading_host, original_ext, original_path, crop_xyz, background_channel, signal_channel, ac1_channel, notes, reference, user_id FROM images_alignment WHERE id = %s", [newName, oldId])
+        cur.connection.commit()
+        gc.collect()
+        cur.execute("SELECT id FROM images_alignment WHERE name = %s", [newName])
+        results = cur.fetchall()
+        newId = results[0][0]
+        gc.collect()
+        print "New ID: " + str(newId)
+        cur.execute("INSERT INTO images_original_nrrd ( image_id, channel, new_min, new_max, file, is_index, pre_hist ) SELECT %s, channel, new_min, new_max, replace(file, %s, %s), is_index, pre_hist FROM images_original_nrrd WHERE image_id = %s", [newId, oldName, newName, oldId])
+        cur.connection.commit()
+        gc.collect()
+        cur.execute("SELECT file, id FROM images_original_nrrd WHERE image_id = %s", [newId])
+        results = cur.fetchall()
+        print 'Duplicating files...'
+        newOrig = line[5]
+        for fl in results:
+            shutil.copyfile(tempfolder + str(fl[0]).replace(newName, oldName),tempfolder + str(fl[0]))
+            if (str(line[2]) == str(fl[0]).replace(newName, oldName)):
+                newOrig = fl[1]
+                print 'file matched'
+        os.rename(tempfolder + modfile, tempfolder + str(modfile).replace(oldName, newName))
+        os.rename(tempfolder + maskfile, tempfolder + str(maskfile).replace(oldName, newName))
+        print 'Switching to new alignment via ' + str(newOrig)
+        cur.execute("UPDATE images_mask_original SET image_id=%s WHERE id = %s ", [newOrig, line[0]])
+        cur.connection.commit()
+        gc.collect()
       if line[3]:
-        print 'Updating with results...'
-        cur.execute("UPDATE images_original_nrrd SET file=%s WHERE id = %s ", [modfile, str(line[5])])
-        cur.connection.commit()
-        gc.collect()
         print 'Auto restarting alignment...'
-        cur.execute("UPDATE images_alignment SET alignment_stage=2002 WHERE id = %s ", [str(line[4])])
+        cur.execute("UPDATE images_alignment SET alignment_stage=2002 WHERE id = %s ", [newId])
         cur.connection.commit()
         gc.collect()
-        try:
-          os.chmod(tempfolder + str(line[2]), stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
-        except:
-          pass
+      try:
+        os.chmod((tempfolder + modfile), stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
+      except:
+        pass
     print 'done'
   else:
     print 'inactive or stage 0 not selected'
