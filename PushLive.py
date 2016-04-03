be76588bef22c540912fe7766b7fa06129b4bb7a
Rob Court <robbie.court@gmail.com>
2016-04-03 06:58:16 +0100

    syntax fix

--- PushLive.py
+++ PushLive.py
@@ -61,9 +61,9 @@ def pushLive(id, name, sgfile):
             print "Clearing old woolz data..."
         
         print "Creating wlz: " + "/VFB/i/" + first + "/" + last + "/volume.wlz"
-        print "Using voxel sizes: Z:" + head['space directions'][2][2] + ", X:" + head['space directions'][0][0] + ", Y:" + head['space directions'][1][1]
+        print "Using voxel sizes: Z:" + str(head['space directions'][2][2]) + ", X:" + str(head['space directions'][0][0]) + ", Y:" + str(head['space directions'][1][1])
         
-        subprocess.call("nice " + wlzDir + "WlzExtFFConvert -f tif -F wlz " + tempfolder + "../../IMAGE_DATA/VFB/i/" + first + "/" + last + "/volume.tif |" + wlzDir + "WlzThreshold -v50 |" + wlzDir + "WlzSetVoxelSize -z" + head['space directions'][2][2] + " -x" + head['space directions'][0][0] + " -y" + head['space directions'][1][1] + " >" + tempfolder + "../../IMAGE_DATA/VFB/i/" + first + "/" + last + "/volume.wlz" , shell=True)
+        subprocess.call("nice " + wlzDir + "WlzExtFFConvert -f tif -F wlz " + tempfolder + "../../IMAGE_DATA/VFB/i/" + first + "/" + last + "/volume.tif |" + wlzDir + "WlzThreshold -v50 |" + wlzDir + "WlzSetVoxelSize -z" + str(head['space directions'][2][2]) + " -x" + str(head['space directions'][0][0]) + " -y" + str(head['space directions'][1][1]) + " >" + tempfolder + "../../IMAGE_DATA/VFB/i/" + first + "/" + last + "/volume.wlz" , shell=True)
         
         cur.execute("UPDATE images_alignment SET alignment_stage = 21, notes = %s WHERE id = %s", [str("VFB_"+first+last),str(id)])
         cur.connection.commit()
